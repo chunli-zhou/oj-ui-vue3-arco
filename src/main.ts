703fdbe985c3b02fa6a757ba41e35e7da4c51f52
张瑞鑫 <zrx15726109130@outlook.com>
2024-04-21 22:09:18 +0800

    🐛fix：修复组件库样式确实问题

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,9 @@ import '@/api/interceptor';
 import ArcoVueIcon from '@arco-design/web-vue/es/icon';
 import '@/assets/style/tailwind.css';
 import 'md-editor-v3/lib/style.css';
-// import '@arco-design/web-vue/es/message/style/index.less';
-// import '@arco-design/web-vue/es/notification/style/index.less';
-// import '@arco-design/web-vue/es/modal/style/index.less';
+import '@arco-design/web-vue/es/message/style/index.less';
+import '@arco-design/web-vue/es/notification/style/index.less';
+import '@arco-design/web-vue/es/modal/style/index.less';
 
 const app = createApp(App);
 
